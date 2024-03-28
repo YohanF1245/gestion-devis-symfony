2b1909c45ee784a1f983ae9355441ad8206e33a3
Yohan F <yohan.ferdin@hotmail.fr>
2024-03-28 15:35:41 +0100

    created form controller to push signup data into db

---
+++ src/Controller/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use App\Form\CreateUserType;
+use DateTime;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+
+    #[Route("/user", name: "create_user")]
+    public function createUser(EntityManagerInterface $entityManager): Response
+    {
+        $user = new Users();
+        $user->setMail("test@test.com");
+        $user->setPass("devine");
+        $user->setPseudo("toto");
+        $user->setCreationDate(new DateTime("now"));
+
+        $entityManager->persist($user);
+
+        $entityManager->flush();
+
+        return new Response("Utilisateur créé avec l'id : ".$user->getId());
+    }
+
+    #[Route("/user/1", name: "show_user")]
+    public function show(EntityManagerInterface $entityManager, string $pseudo )
+    {
+        $user = $entityManager->getRepository(Users::class)->find($pseudo);
+        if(!$user){
+            throw $this->createNotFoundException("no user foudn");
+        }
+        return new Response("user create at : ".$user->getCreationDate());
+    }
+    #[Route("/user/create/form", name:"user.create")]
+    public function signup(Request $request, EntityManagerInterface $em){
+        $form = $this->createForm(CreateUserType::class);
+        $form->handleRequest(($request));
+
+        if($form->isSubmitted() && $form->isValid()){
+            $data = new Users();
+            $data = $form->getData();
+            $data->setCreationDate(new DateTime("now"));
+              $em->persist($data);
+             $em->flush();
+        }
+
+        return $this->render("user/signup.html.twig", [
+            "form"=>$form,
+        ]);
+     }
+}
---
+++ src/Form/CreateUserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ResetPass;
+use App\Entity\Users;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('mail')
+            ->add('signature')
+            ->add('pass')
+            ->add('pseudo')
+            ->add('Envoyer', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
