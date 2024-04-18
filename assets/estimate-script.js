c63cf6a10d602bf89f0eb8423accceef14ee0f32
YohanF1245 <yohan.ferdin@hotmail.fr>
2024-04-18 16:32:49 +0200

    added remove last item function

--- assets/estimate-script.js
+++ assets/estimate-script.js
@@ -1,18 +1,24 @@
-function addPerformance(){
+function addPerformance() {
     const perfDiv = document.getElementById("perfomanceTableBody");
     const perfObject = document.getElementById("selectPerf").value;
-    const perfArray = perfObject.split('!');
-    let quantity = perfArray[1];
-    let designation = perfArray[2];
-    let price = perfArray[3];
-    let unit = perfArray[4];
-    console.log(perfArray);
-    const test = document.createElement("tr");
-    test.setAttribute("value",perfArray[0])
-    test.innerHTML = "<td>"+quantity+"</td><td>"+designation+"</td><td>"+price+"</td><td>"+unit+"</td>";
-    perfDiv.append(test);
+    if (perfObject !== "Ajouter une prestation") {
+        const perfArray = perfObject.split('!');
+        let quantity = perfArray[1];
+        let designation = perfArray[2];
+        let price = perfArray[3];
+        let unit = perfArray[4];
+        console.log(perfArray);
+        const test = document.createElement("tr");
+        test.setAttribute("value", perfArray[0])
+        test.innerHTML = "<td>" + quantity + "</td><td>" + designation + "</td><td>" + price + "</td><td>" + unit + "</td>";
+        perfDiv.append(test);
+    }
 }
 
+function removeLast() {
+    var select = document.getElementById('perfomanceTableBody');
+    select.removeChild(select.lastChild);
+}
 // {
 //     "indexArray" : 1,
 //     "quantity" : 32,
