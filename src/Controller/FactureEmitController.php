14107af422ea357bce4c80266aa1ebc793e8da0b
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-06-21 21:10:03 +0200

    created invoice crud

---
+++ src/Controller/FactureEmitController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\FactureEmit;
+use App\Form\FactureEmitType;
+use App\Repository\FactureEmitRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/facture/emit')]
+class FactureEmitController extends AbstractController
+{
+    #[Route('/', name: 'app_facture_emit_index', methods: ['GET'])]
+    public function index(FactureEmitRepository $factureEmitRepository): Response
+    {
+        return $this->render('facture_emit/index.html.twig', [
+            'facture_emits' => $factureEmitRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_facture_emit_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $factureEmit = new FactureEmit();
+        $form = $this->createForm(FactureEmitType::class, $factureEmit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($factureEmit);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_facture_emit_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('facture_emit/new.html.twig', [
+            'facture_emit' => $factureEmit,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_facture_emit_show', methods: ['GET'])]
+    public function show(FactureEmit $factureEmit): Response
+    {
+        return $this->render('facture_emit/show.html.twig', [
+            'facture_emit' => $factureEmit,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_facture_emit_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, FactureEmit $factureEmit, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(FactureEmitType::class, $factureEmit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_facture_emit_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('facture_emit/edit.html.twig', [
+            'facture_emit' => $factureEmit,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_facture_emit_delete', methods: ['POST'])]
+    public function delete(Request $request, FactureEmit $factureEmit, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$factureEmit->getId(), $request->getPayload()->get('_token'))) {
+            $entityManager->remove($factureEmit);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_facture_emit_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/FactureEmitType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\EstimateTab;
+use App\Entity\FactureEmit;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FactureEmitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('creation_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('payment_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('majoration')
+            ->add('date_limit', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('is_paid')
+            ->add('estimateTab', EntityType::class, [
+                'class' => EstimateTab::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => FactureEmit::class,
+        ]);
+    }
+}
