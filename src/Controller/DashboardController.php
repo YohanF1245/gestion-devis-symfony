c04714e578e05f3d196a690574af055e335e6773
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-06-23 00:00:15 +0200

    added last devices logic on dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Entity\Business;
 use App\Entity\Users;
+use App\Repository\DressEstimateRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,13 +12,26 @@ use Symfony\Component\Routing\Attribute\Route;
 class DashboardController extends AbstractController {
 
     #[Route("/dashboard", name: "home")]
-    function dashboard(Request $request, EntityManagerInterface $entityManager){
+    function dashboard(Request $request,DressEstimateRepository $dressEstimateRepository, EntityManagerInterface $entityManager){
         $user = $this->getUser();
         $userId = $user->getId();
+
+        $estimateList = $dressEstimateRepository->findBy([
+        'user_id' => $userId],
+        ['creation_date' => 'ASC'
+    ]);	
+
+
         $repository = $entityManager->getRepository(Business::class);
-        $business = $repository->findOneBy(['user_id' => $userId]);
+        $business = $repository->findOneBy(
+            ['user_id' => $userId]);
         
-        return $this->render("dashboard.html.twig", ['business' => $business]);
+        return $this->render("dashboard.html.twig",
+
+             ['business' => $business,
+             'estimate_list' => $estimateList]
+            
+            );
     }
 
 }
